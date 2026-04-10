29d782d467faeac32b54e86ce04aa64cc2303037
jhon <jhonhuaman65432@gmail.com>
2026-04-10 10:43:50 -0500

    subiendo ejer1 de py

---
+++ vercion2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3
+{
+    internal class ejer1
+    {
+        static void main(string[] args)
+        {
+            string n, c;
+
+            Console.WriteLine("ingrese nombre: ");
+            n = Console.ReadLine();
+            Console.WriteLine("ingrese carrera: ");
+            n = Console.ReadLine();
+
+            Console.WriteLine("\n-----imprecion con + ------");
+            Console.WriteLine(n + ", bienvenido al curso FA de la carrera"+c);
+
+            Console.WriteLine("\n-----imprecion con + ------");
+            Console.WriteLine($"{n}, Bienvenido al curso FA de la carrera {c}");
+            Console.ReadKey();
+        }
+    }
+}
