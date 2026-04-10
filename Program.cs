ba9f7c0ec66289523f5b6f042ef9603a81941dde
jhon <jhonhuaman65432@gmail.com>
2026-04-10 10:27:49 -0500

    subiendo ejer1 en c#

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+
+
+using System;
+
+namespace se3_c
+{
+    internal class ejer1
+    {
+        static void main(string[] args)
+        {
+            string n, c;
+
+            Console.WriteLine("ingrese nombre: ");
+            n = Console.ReadLine();
+            Console.WriteLine("ingrese carrera: ");
+            n = Console.ReadLine();
+
+            Console.WriteLine("\n-----imprecion con + ------");
+            Console.WriteLine(n + ", bienvenido al curso FA de la carrera"+c);
+
+            Console.WriteLine("\n-----imprecion con + ------");
+            Console.WriteLine($"{n}, Bienvenido al curso FA de la carrera {c}");
+            Console.ReadKey();
+        }
+    }
+}
